b32238fd9d3aaf2cb586c430bdfc3aa69d61add7
ruanout1 <ruanout@gmail.com>
2025-09-10 15:50:46 -0300

    Commit Incial

---
+++ internal/hello/hello.go
@@ -0,0 +1,12 @@
+// Pacote interno (internal)
+// Convenção de mercado: colocar lógicas que não serão exportadas para outros projetos
+package hello  
+
+// Importa o pacote fmt para formatação de strings e impressão
+import "fmt"
+
+// Função simples para demonstrar organização do código
+func SayHello() {
+    // Imprime uma mensagem no console
+    fmt.Println("Olá, mundo! FacINpro! 👋")
+}
