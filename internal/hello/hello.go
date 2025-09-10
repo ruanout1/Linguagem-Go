5e0b8ab4359f33e6344ece790633b6faaf8fc202
ruanout1 <ruanout@gmail.com>
2025-09-10 16:12:12 -0300

    atividade 1

--- internal/hello/hello.go
+++ internal/hello/hello.go
@@ -1,12 +1,12 @@
 // Pacote interno (internal)
 // Convenção de mercado: colocar lógicas que não serão exportadas para outros projetos
-package hello  
+package hello
 
 // Importa o pacote fmt para formatação de strings e impressão
 import "fmt"
 
 // Função simples para demonstrar organização do código
 func SayHello() {
-    // Imprime uma mensagem no console
-    fmt.Println("Olá, mundo! FacINpro! 👋")
+	// Imprime uma mensagem no console
+	fmt.Println("Olá, mundo! FacINpro! Ruan Almeida Souza 👋")
 }
